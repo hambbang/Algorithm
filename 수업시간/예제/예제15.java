46794ee08a4e532c44932d46ed0714d4c36c08ab
CHOI YOONWOO <114982748+hambbang@users.noreply.github.com>
2023-12-20 13:49:09 +0900

    예제15-수업시간

---
+++ 수업시간/예제/예제15.java
@@ -0,0 +1,77 @@
+package Hambbang;
+
+import java.util.Scanner;
+
+class Check {
+	Scanner sc = new Scanner(System.in);
+	String num;
+	int hn;
+	String hName[];
+	int 김 = 0;
+	int 이 = 0;
+	int 박 = 0;
+	int 최 = 0;
+	int 정 = 0;
+	int most = 0;
+	char 최다성;
+
+	Check() {
+		Ask1();
+	}
+
+	void Ask1() {
+		System.out.print("몇 명이 있습니까? ");
+		num = sc.next();
+		hn = Integer.parseInt(num.substring(0,1));
+		hName = new String[hn];
+		for (int i = 0; i < hn; i++) {
+			System.out.println(i + 1 + "번 이름을 입력하시오: ");
+			hName[i] = sc.next();
+			switch (hName[i].charAt(0)) {
+			case '김':
+				김++;
+				break;
+			case '이':
+				이++;
+				break;
+			case '박':
+				박++;
+				break;
+			case '최':
+				최++;
+				break;
+			case '정':
+				정++;
+				break;
+			}
+		}
+		System.out.println("김씨는 " + 김 + "명/이씨는 " + 이 + "명/박씨는 " + 박 + "명/정씨는 " + 정 + "명/최씨는 " + 최 + "명");
+		if (most < 김) {
+			최다성 = '김';
+			most = 김;
+		}
+		if (most < 이) {
+			최다성 = '이';
+			most = 이;
+		}
+		if (most < 박) {
+			최다성 = '박';
+			most = 박;
+		}
+		if (most < 정) {
+			최다성 = '정';
+			most = 정;
+		}
+		if (most < 최) {
+			최다성 = '최';
+			most = 최;
+		}
+		System.out.println("제일 많은 성은 "+최다성+"씨 "+most+"명입니다.");
+	}
+}
+
+public class Hamtua {
+	public static void main(String[] args) {
+		Check test = new Check();
+	}
+}
